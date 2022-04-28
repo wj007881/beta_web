1d6cf2722c0d1c57c443577f11908fb11917a09a
嗷呜嗷呜 <425547052@qq.com>
2022-04-28 18:19:30 +0800

    daily save 2022/04/28

--- vite.config.js
+++ vite.config.js
@@ -34,6 +34,7 @@ export default defineConfig(({ command, mode }) => {
     server: {
       host: '0.0.0.0',
       port: VITE_PORT,
+      https:true,
       proxy: createProxy(VITE_PROXY),
     },
     build: {
